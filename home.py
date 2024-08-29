ed8f60d28b828d94b97144a416cd2fdf9bb6e205
the1am <116252893+the1am@users.noreply.github.com>
2024-08-29 13:39:10 +0800

    Create home.py

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+path_to_html = "./customized.html" 
+
+# Read file and keep in variable
+with open(path_to_html,'r') as f: 
+    html_data = f.read()
+
+## Show in webpage
+st.header("Show an external HTML")
+st.components.v1.html(html_data,height=200)
